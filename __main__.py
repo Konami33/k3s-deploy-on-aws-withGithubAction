005ec32fcb5bdd58b03c4e9adaf27b6e519e0cac
Konami33 <yasinarafat9889@gmail.com>
2024-06-29 03:32:01 +0600

    final - 3

--- __main__.py
+++ __main__.py
@@ -134,10 +134,58 @@ vpc = aws.ec2.Vpc("my-vpc",
     enable_dns_support=True,
 )
 
-subnet = aws.ec2.Subnet("my-subnet",
+# Create Public Subnet
+public_subnet = aws.ec2.Subnet("public-subnet",
     vpc_id=vpc.id,
     cidr_block="10.0.1.0/24",
-    availability_zone="ap-southeast-1a"
+    map_public_ip_on_launch=True,
+    availability_zone="ap-southeast-1a",
+)
+
+# Create Internet Gateway
+igw = aws.ec2.InternetGateway("igw",
+    vpc_id=vpc.id,
+)
+
+# Create Route Table
+route_table = aws.ec2.RouteTable("route-table",
+    vpc_id=vpc.id,
+    routes=[{
+        "cidr_block": "0.0.0.0/0",
+        "gateway_id": igw.id,
+    }],
+)
+
+# Associate Route Table with Public Subnet
+rt_assoc_public = aws.ec2.RouteTableAssociation("rt-assoc-public",
+    subnet_id=public_subnet.id,
+    route_table_id=route_table.id,
+)
+
+# Create Security Group
+security_group = aws.ec2.SecurityGroup("web-secgrp",
+    description='Enable SSH and K3s access',
+    vpc_id=vpc.id,
+    ingress=[
+        {
+            "protocol": "tcp",
+            "from_port": 22,
+            "to_port": 22,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+        {
+            "protocol": "tcp",
+            "from_port": 6443,
+            "to_port": 6443,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+    ],
+    egress=[{
+        "protocol": "-1",
+        "from_port": 0,
+        "to_port": 0,
+        "cidr_blocks": ["0.0.0.0/0"],
+    }],
 )
 
 # Create instances in the VPC and subnet
@@ -147,7 +195,7 @@ instance_type = "t2.micro"
 master_node = aws.ec2.Instance("master-node",
     instance_type=instance_type,
     ami=ami_id,
-    subnet_id=subnet.id,
+    subnet_id=public_subnet.id,
     key_name=key_pair.key_name,
     tags={
         "Name": "master-node"
@@ -156,7 +204,7 @@ master_node = aws.ec2.Instance("master-node",
 worker_node_1 = aws.ec2.Instance("worker-node-1",
     instance_type=instance_type,
     ami=ami_id,
-    subnet_id=subnet.id,
+    subnet_id=public_subnet.id,
     key_name=key_pair.key_name,
     tags={
         "Name": "worker-node-1"
@@ -165,7 +213,7 @@ worker_node_1 = aws.ec2.Instance("worker-node-1",
 worker_node_2 = aws.ec2.Instance("worker-node-2",
     instance_type=instance_type,
     ami=ami_id,
-    subnet_id=subnet.id,
+    subnet_id=public_subnet.id,
     key_name=key_pair.key_name,
     tags={
         "Name": "worker-node-2"
